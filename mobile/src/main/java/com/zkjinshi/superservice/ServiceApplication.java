f1191e0012beb57f79b4d29a74d210e57be63a59
JimmyZhang <jimmyzhang144@gmail.com>
2015-09-21 10:44:20 +0800

    初始化提交超级服务项目

---
+++ mobile/src/main/java/com/zkjinshi/superservice/ServiceApplication.java
@@ -0,0 +1,18 @@
+package com.zkjinshi.superservice;
+
+import android.app.Application;
+
+/**
+ * 超级服务入口
+ * 开发者：JimmyZhang
+ * 日期：2015/9/21
+ * Copyright (C) 2015 深圳中科金石科技有限公司
+ * 版权所有
+ */
+public class ServiceApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
