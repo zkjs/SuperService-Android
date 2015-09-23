e8e529b568026139aad07206202a834ff085e3f9
JimmyZhang <jimmyzhang144@gmail.com>
2015-09-23 15:04:24 +0800

    新增配置项

--- mobile/src/main/java/com/zkjinshi/superservice/activity/common/MainActivity.java
+++ mobile/src/main/java/com/zkjinshi/superservice/activity/common/MainActivity.java
@@ -2,6 +2,7 @@ package com.zkjinshi.superservice.activity.common;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 
 import com.zkjinshi.superservice.R;
@@ -13,11 +14,27 @@ import com.zkjinshi.superservice.R;
  * Copyright (C) 2015 深圳中科金石科技有限公司
  * 版权所有
  */
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+    private void initView(){
+
+    }
+
+    private void initData(){
+
+    }
+
+    private void initListeners(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+        initListeners();
     }
+
 }
