ac254fc652add454b0fa63adc6ae6967891a4517
JimmyZhang <jimmyzhang144@gmail.com>
2015-09-23 14:37:24 +0800

    新增启动应用页面

--- mobile/src/main/java/com/zkjinshi/superservice/activity/common/SplashActivity.java
+++ mobile/src/main/java/com/zkjinshi/superservice/activity/common/SplashActivity.java
@@ -3,21 +3,20 @@ package com.zkjinshi.superservice.activity.common;
 import android.app.Activity;
 import android.os.Bundle;
 
-
 import com.zkjinshi.superservice.R;
 
 /**
- * 主页面
+ * 应用启动页面
  * 开发者：JimmyZhang
  * 日期：2015/9/23
  * Copyright (C) 2015 深圳中科金石科技有限公司
  * 版权所有
  */
-public class MainActivity extends Activity {
+public class SplashActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
