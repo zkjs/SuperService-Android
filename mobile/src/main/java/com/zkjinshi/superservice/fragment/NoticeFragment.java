fc16596372e91f23ae511ff78471ad4e226eabd9
JimmyZhang <jimmyzhang144@gmail.com>
2015-09-23 14:34:13 +0800

    新增到店通知、消息通知、订单处理3个Fragment

---
+++ mobile/src/main/java/com/zkjinshi/superservice/fragment/NoticeFragment.java
@@ -0,0 +1,44 @@
+package com.zkjinshi.superservice.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 到店通知
+ * 开发者：JimmyZhang
+ * 日期：2015/9/23
+ * Copyright (C) 2015 深圳中科金石科技有限公司
+ * 版权所有
+ */
+public class NoticeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
