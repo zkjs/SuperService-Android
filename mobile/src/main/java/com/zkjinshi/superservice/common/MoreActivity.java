5a109c3eb2e8787bb4373c4bc7399a271da256e2
dujiande <jiande@zkjinshi.com>
2015-09-23 14:09:23 +0800

    设置相册

--- mobile/src/main/java/com/zkjinshi/superservice/common/MoreActivity.java
+++ mobile/src/main/java/com/zkjinshi/superservice/common/MoreActivity.java
@@ -1,12 +1,24 @@
 package com.zkjinshi.superservice.common;
 
-import android.app.Activity;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 
 import com.zkjinshi.superservice.R;
+import com.zkjinshi.superservice.view.CircleImageView;
+
+import java.io.File;
+import java.util.List;
+
+import me.nereo.multi_image_selector.MultiImageSelectorActivity;
+import me.nereo.multi_image_selector.MultiImageSelectorFragment;
 
 /**
  * 开发者：dujiande
@@ -14,22 +26,57 @@ import com.zkjinshi.superservice.R;
  * Copyright (C) 2015 深圳中科金石科技有限公司
  * 版权所有
  */
-public class MoreActivity extends Activity {
+public class MoreActivity extends FragmentActivity implements MultiImageSelectorFragment.Callback{
 
     private final static String TAG = MoreActivity.class.getSimpleName();
+
+    private CircleImageView avatarCiv;
+
+    public static int REQUEST_IMAGE = 1;
+
+    @Override
+    public void onSingleImageSelected(String path) {
+        // 当选择模式设定为 单选/MODE_SINGLE, 这个方法就会接受到Fragment返回的数据
+    }
+
+    @Override
+    public void onImageSelected(String path) {
+        // 一个图片被选择是触发，这里可以自定义的自己的Actionbar行为
+    }
+
+    @Override
+    public void onImageUnselected(String path) {
+        // 一个图片被反选是触发，这里可以自定义的自己的Actionbar行为
+    }
+
+    @Override
+    public void onCameraShot(File imageFile) {
+        // 当设置了使用摄像头，用户拍照后会返回照片文件
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more);
-
+        initFragment();
         initView();
         initData();
         initListener();
     }
 
+    private void initFragment() {
+        Bundle bundle = new Bundle();
+        bundle.putInt(MultiImageSelectorFragment.EXTRA_SELECT_COUNT, 1);
+        bundle.putInt(MultiImageSelectorFragment.EXTRA_SELECT_MODE,  MultiImageSelectorActivity.MODE_SINGLE);
+        bundle.putBoolean(MultiImageSelectorActivity.EXTRA_SHOW_CAMERA, true);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.image_grid, Fragment.instantiate(this, MultiImageSelectorFragment.class.getName(), bundle))
+                .commit();
+    }
+
     private void initView() {
+        avatarCiv = (CircleImageView)findViewById(R.id.avatar);
 
     }
 
@@ -55,5 +102,39 @@ public class MoreActivity extends Activity {
                 overridePendingTransition(R.anim.activity_new, R.anim.activity_out);
             }
         });
+
+        avatarCiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MoreActivity.this, MultiImageSelectorActivity.class);
+                // 是否显示调用相机拍照
+                intent.putExtra(MultiImageSelectorActivity.EXTRA_SHOW_CAMERA, true);
+                // 最大图片选择数量
+                intent.putExtra(MultiImageSelectorActivity.EXTRA_SELECT_COUNT, 1);
+                // 设置模式 (支持 单选/MultiImageSelectorActivity.MODE_SINGLE 或者 多选/MultiImageSelectorActivity.MODE_MULTI)
+                intent.putExtra(MultiImageSelectorActivity.EXTRA_SELECT_MODE, MultiImageSelectorActivity.MODE_SINGLE);
+                startActivityForResult(intent, REQUEST_IMAGE);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE){
+            if(resultCode == RESULT_OK){
+                // 获取返回的图片列表
+                List<String> path = data.getStringArrayListExtra(MultiImageSelectorActivity.EXTRA_RESULT);
+                Log.e(TAG,path.toString());
+
+                String photoFilePath = path.get(0);
+                Bitmap displayBitmap = BitmapFactory.decodeFile(photoFilePath);
+                if (displayBitmap != null) {
+                    //displayBitmap = ImageUtil.cropThumbBitmap(displayBitmap);
+                    //displayBitmap = ImageUtil.loadThumbBitmap(MoreActivity.this, displayBitmap);
+                    avatarCiv.setImageBitmap(displayBitmap);
+                }
+               // CacheUtil.getInstance().savePicPath(photoFilePath);
+            }
+        }
     }
 }
