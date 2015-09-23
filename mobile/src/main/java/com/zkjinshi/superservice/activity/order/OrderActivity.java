cab504b614b870fc54ffc517931629b8ff53569c
JimmyZhang <jimmyzhang144@gmail.com>
2015-09-23 14:42:53 +0800

    新增聊天、订单、设置主页面

---
+++ mobile/src/main/java/com/zkjinshi/superservice/activity/order/OrderActivity.java
@@ -0,0 +1,20 @@
+package com.zkjinshi.superservice.activity.order;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zkjinshi.superservice.R;
+
+/**
+ * 开发者：JimmyZhang
+ * 日期：2015/9/23
+ * Copyright (C) 2015 深圳中科金石科技有限公司
+ * 版权所有
+ */
+public class OrderActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
---
+++ mobile/src/main/java/com/zkjinshi/superservice/activity/set/SetActivity.java
@@ -0,0 +1,21 @@
+package com.zkjinshi.superservice.activity.set;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zkjinshi.superservice.R;
+
+/**
+ * 开发者：JimmyZhang
+ * 日期：2015/9/23
+ * Copyright (C) 2015 深圳中科金石科技有限公司
+ * 版权所有
+ */
+public class SetActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set);
+    }
+}
