4cafb7f172c1e43eadc648b9e075e35ee865a452
JimmyZhang <jimmyzhang144@gmail.com>
2015-09-24 17:24:02 +0800

    通过代理模式，抽出MainActivity的核心实现

--- mobile/src/main/java/com/zkjinshi/superservice/activity/common/MainActivity.java
+++ mobile/src/main/java/com/zkjinshi/superservice/activity/common/MainActivity.java
@@ -29,189 +29,28 @@ import com.zkjinshi.superservice.view.Fab;
  * Copyright (C) 2015 深圳中科金石科技有限公司
  * 版权所有
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private ActionBarDrawerToggle drawerToggle;
-    private DrawerLayout drawerLayout;
-    private MaterialSheetFab materialSheetFab;
-    private int statusBarColor;
+    private MainActivityController mainActivityController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle(R.string.app_name);
         setContentView(R.layout.activity_main);
-        setupActionBar();
-        setupDrawer();
-        setupFab();
-        setupTabs();
+        mainActivityController = new MainActivityController(this);
+        mainActivityController.onCreate();
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
+        mainActivityController.onPostCreate();
     }
 
     @Override
     public void onBackPressed() {
-        if (materialSheetFab.isSheetVisible()) {
-            materialSheetFab.hideSheet();
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    /**
-     * Sets up the action bar.
-     */
-    private void setupActionBar() {
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    /**
-     * 设置导航侧滑栏
-     */
-    private void setupDrawer() {
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.opendrawer,
-                R.string.closedrawer);
-        drawerLayout.setDrawerListener(drawerToggle);
-    }
-
-    /**
-     * 设置tabs.
-     */
-    private void setupTabs() {
-        // 设置ViewPager
-        ViewPager viewpager = (ViewPager) findViewById(R.id.viewpager);
-        viewpager.setAdapter(new ViewPagerAdapter(this, getSupportFragmentManager()));
-        viewpager.setOffscreenPageLimit(ViewPagerAdapter.NUM_ITEMS);
-        updatePage(viewpager.getCurrentItem());
-
-        // 设置导航菜单
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewpager);
-        viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int i, float v, int i1) {
-            }
-
-            @Override
-            public void onPageSelected(int i) {
-                updatePage(i);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int i) {
-            }
-        });
-    }
-
-    /**
-     * 设置悬浮菜单按钮
-     */
-    private void setupFab() {
-
-        Fab fab = (Fab) findViewById(R.id.fab);
-        View sheetView = findViewById(R.id.fab_sheet);
-        View overlay = findViewById(R.id.overlay);
-        int sheetColor = getResources().getColor(R.color.background_card);
-        int fabColor = getResources().getColor(R.color.theme_accent);
-
-        // Create material sheet FAB
-        materialSheetFab = new MaterialSheetFab<>(fab, sheetView, overlay, sheetColor, fabColor);
-
-        // Set material sheet event listener
-        materialSheetFab.setEventListener(new MaterialSheetFabEventListener() {
-            @Override
-            public void onShowSheet() {
-                // Save current status bar color
-                statusBarColor = getStatusBarColor();
-                // Set darker status bar color to match the dim overlay
-                setStatusBarColor(getResources().getColor(R.color.theme_primary_dark2));
-            }
-
-            @Override
-            public void onHideSheet() {
-                // Restore status bar color
-                setStatusBarColor(statusBarColor);
-            }
-        });
-
-        // Set material sheet item click listeners
-        findViewById(R.id.fab_sheet_item_recording).setOnClickListener(this);
-        findViewById(R.id.fab_sheet_item_reminder).setOnClickListener(this);
-        findViewById(R.id.fab_sheet_item_photo).setOnClickListener(this);
-        findViewById(R.id.fab_sheet_item_note).setOnClickListener(this);
-    }
-
-    /**
-     * Called when the selected page changes.
-     *
-     * @param selectedPage selected page
-     */
-    private void updatePage(int selectedPage) {
-        updateFab(selectedPage);
-        updateSnackbar(selectedPage);
-    }
-
-    /**
-     * Updates the FAB based on the selected page
-     *
-     * @param selectedPage selected page
-     */
-    private void updateFab(int selectedPage) {
-        switch (selectedPage) {
-            case ViewPagerAdapter.ALL_POS:
-                materialSheetFab.showFab();
-                break;
-            case ViewPagerAdapter.SHARED_POS:
-                materialSheetFab.showFab(0,
-                        -getResources().getDimensionPixelSize(R.dimen.snackbar_height));
-                break;
-            case ViewPagerAdapter.FAVORITES_POS:
-            default:
-                materialSheetFab.hideSheetThenFab();
-                break;
-        }
-    }
-
-    /**
-     * Updates the snackbar based on the selected page
-     *
-     * @param selectedPage selected page
-     */
-    private void updateSnackbar(int selectedPage) {
-        View snackbar = findViewById(R.id.snackbar);
-        switch (selectedPage) {
-            case ViewPagerAdapter.SHARED_POS:
-                snackbar.setVisibility(View.VISIBLE);
-                break;
-            case ViewPagerAdapter.ALL_POS:
-            case ViewPagerAdapter.FAVORITES_POS:
-            default:
-                snackbar.setVisibility(View.GONE);
-                break;
-        }
-    }
-
-    /**
-     * Toggles opening/closing the drawer.
-     */
-    private void toggleDrawer() {
-        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            drawerLayout.openDrawer(GravityCompat.START);
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(this, R.string.sheet_item_pressed, Toast.LENGTH_SHORT).show();
-        materialSheetFab.hideSheet();
+        mainActivityController.onBackPressed();
     }
 
     @Override
@@ -225,23 +64,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                toggleDrawer();
+                mainActivityController.toggleDrawer();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private int getStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return getWindow().getStatusBarColor();
-        }
-        return 0;
-    }
-
-    private void setStatusBarColor(int color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(color);
         }
     }
 
