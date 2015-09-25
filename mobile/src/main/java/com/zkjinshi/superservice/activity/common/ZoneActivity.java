454372ad91b04be05e815078eea9bc7e8bc51d97
dujiande <jiande@zkjinshi.com>
2015-09-25 14:44:28 +0800

    1设置区域通知列表添加下拉刷新和加载更多 2左侧菜单增加点击效果

--- mobile/src/main/java/com/zkjinshi/superservice/activity/common/ZoneActivity.java
+++ mobile/src/main/java/com/zkjinshi/superservice/activity/common/ZoneActivity.java
@@ -3,6 +3,7 @@ package com.zkjinshi.superservice.activity.common;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 import android.view.Window;
 import android.widget.ListView;
@@ -10,6 +11,8 @@ import android.widget.ListView;
 import com.zkjinshi.superservice.R;
 import com.zkjinshi.superservice.adapter.ZoneAdapter;
 import com.zkjinshi.superservice.test.ZoneBiz;
+import com.zkjinshi.superservice.view.RefreshLayout;
+
 
 /**
  * 开发者：dujiande
@@ -17,11 +20,14 @@ import com.zkjinshi.superservice.test.ZoneBiz;
  * Copyright (C) 2015 深圳中科金石科技有限公司
  * 版权所有
  */
-public class ZoneActivity extends Activity{
+public class ZoneActivity extends Activity implements SwipeRefreshLayout.OnRefreshListener,RefreshLayout.OnLoadListener {
 
     private final static String TAG = ZoneActivity.class.getSimpleName();
+
+    private RefreshLayout swipeLayout;
     private ListView zoneLv;
     private ZoneAdapter zoneAdapter;
+    private View header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,11 @@ public class ZoneActivity extends Activity{
     }
 
     private void initView() {
+        header = getLayoutInflater().inflate(R.layout.header, null);
+        swipeLayout = (RefreshLayout) findViewById(R.id.swipe_container);
+        swipeLayout.setOnRefreshListener(this);
+
+
         zoneLv = (ListView)findViewById(R.id.zone_listview);
         zoneAdapter = new ZoneAdapter(this, ZoneBiz.getZoneList());
         zoneLv.setAdapter(zoneAdapter);
@@ -45,6 +56,9 @@ public class ZoneActivity extends Activity{
     }
 
     private void initListener() {
+        swipeLayout.setOnRefreshListener(this);
+        swipeLayout.setOnLoadListener(this);
+
         findViewById(R.id.back_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,5 +76,32 @@ public class ZoneActivity extends Activity{
                 overridePendingTransition(R.anim.activity_new, R.anim.activity_out);
             }
         });
+    }
+
+    @Override
+    public void onRefresh() {
+        swipeLayout.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // 更新数据
+                // 更新完后调用该方法结束刷新
+                swipeLayout.setRefreshing(false);
+            }
+        }, 1000);
+
+    }
+
+    @Override
+    public void onLoad() {
+        swipeLayout.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // 更新数据
+                // 更新完后调用该方法结束刷新
+                swipeLayout.setLoading(false);
+            }
+        }, 1000);
     }
 }
