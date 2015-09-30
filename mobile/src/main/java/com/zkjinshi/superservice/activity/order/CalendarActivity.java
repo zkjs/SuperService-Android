5b232a26a908fcee31096576f6c0cc8ef2ea4848
dujiande <jiande@zkjinshi.com>
2015-09-30 15:24:32 +0800

    订单处理标签操作与日期选择

---
+++ mobile/src/main/java/com/zkjinshi/superservice/activity/order/CalendarActivity.java
@@ -0,0 +1,152 @@
+package com.zkjinshi.superservice.activity.order;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.andexert.calendarlistview.DatePickerController;
+import com.andexert.calendarlistview.DayPickerView;
+import com.andexert.calendarlistview.SimpleMonthAdapter;
+import com.zkjinshi.superservice.R;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * 入住和离开日期选择Activity
+ * 开发者：杜健德
+ * 日期：2015/9/30
+ * Copyright (C) 2015 深圳中科金石科技有限公司
+ * 版权所有
+ */
+public class CalendarActivity extends Activity implements DatePickerController {
+
+    //private ImageButton backIBtn;
+    private ImageButton backBtn;//返回
+    private DayPickerView dayPickerView;
+    private LinearLayout tipsLlyt;
+    private TextView tipsTv;
+    private boolean isChooseOver;
+    private ArrayList<Calendar> calendarList;
+    private Calendar checkInCalendar,checkOutCalendar;
+
+    public static final int CALENDAR_REQUEST_CODE = 5;
+
+    private void initView(){
+
+        backBtn = (ImageButton)findViewById(R.id.back_btn);
+        dayPickerView = (DayPickerView)findViewById(R.id.date_picker_choose_view);
+        tipsLlyt = (LinearLayout)findViewById(R.id.calendar_tips_llyt);
+        tipsTv = (TextView)findViewById(R.id.calendar_tips_tv);
+    }
+
+    private void initData(){
+        dayPickerView.setController(this);
+
+        Intent data = getIntent();
+        Serializable serializable =  data.getSerializableExtra("calendarList");
+        if(serializable != null)
+        {
+            calendarList =  (ArrayList<Calendar>)serializable;
+            SimpleMonthAdapter adapter = (SimpleMonthAdapter)dayPickerView.getAdapter();
+            adapter.setFirstDayAndLastDay(calendarList.get(0), calendarList.get(1));
+            adapter.notifyDataSetChanged();
+
+            int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+            int selectMonth = calendarList.get(0).get(Calendar.MONTH);
+            dayPickerView.setSelection(selectMonth - currentMonth);
+        }
+    }
+
+    private void initListeners(){
+
+        //返回
+       backBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               setResult(RESULT_CANCELED);
+               finish();
+               overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+           }
+       });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        initView();
+        initData();
+        initListeners();
+    }
+
+    @Override
+    public int getMaxYear() {
+        return Calendar.getInstance().get(Calendar.YEAR);
+    }
+
+    @Override
+    public void onDayOfMonthSelected(int year, int month, int day) {
+
+    }
+
+    public void onFirstDaySelected(int year, int month, int day) {
+        calendarList = null;
+        calendarList = new ArrayList<Calendar>();
+        checkInCalendar = Calendar.getInstance();
+        checkInCalendar.set(Calendar.YEAR,year);
+        checkInCalendar.set(Calendar.MONTH,month);
+        checkInCalendar.set(Calendar.DAY_OF_MONTH,day);
+        calendarList.add(checkInCalendar);
+
+        showSelectLastDayTips();
+    }
+    private void showSelectLastDayTips() {
+        Animation scaleAnimation1 = new ScaleAnimation(1.0f, 0.0f,1.0f,0.0f,Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation1.setDuration(200);
+        scaleAnimation1.setFillAfter(true);
+        scaleAnimation1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                tipsTv.setText("请选择离店日期");
+                Animation scaleAnimation2 = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                scaleAnimation2.setDuration(200);
+                scaleAnimation2.setFillAfter(true);
+                tipsLlyt.startAnimation(scaleAnimation2);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        tipsLlyt.startAnimation(scaleAnimation1);
+    }
+
+    public void onLastDaySelected(int year, int month, int day) {
+        checkOutCalendar = Calendar.getInstance();
+        checkOutCalendar.set(Calendar.YEAR, year);
+        checkOutCalendar.set(Calendar.MONTH, month);
+        checkOutCalendar.set(Calendar.DAY_OF_MONTH, day);
+        calendarList.add(checkOutCalendar);
+        //跳转回选择页面
+        Intent inetnt = new Intent();
+        inetnt.putExtra("calendarList", calendarList);
+        setResult(RESULT_OK, inetnt);
+        finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+}
--- mobile/src/main/java/com/zkjinshi/superservice/activity/order/OrderDealActivity.java
+++ mobile/src/main/java/com/zkjinshi/superservice/activity/order/OrderDealActivity.java
@@ -1,17 +1,25 @@
 package com.zkjinshi.superservice.activity.order;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.zkjinshi.base.util.TimeUtil;
 import com.zkjinshi.superservice.R;
 import com.zkjinshi.superservice.view.ItemUserSettingView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
+import me.kaede.tagview.Tag;
 import me.kaede.tagview.TagView;
 
 /**
@@ -35,6 +43,14 @@ public class OrderDealActivity extends Activity {
     private TextView        mTvLeaveDate;
     private TextView        mTvDateTips;
     private LinearLayout    mLltDateContainer;
+
+    private SimpleDateFormat mSimpleFormat;
+    private SimpleDateFormat mChineseFormat;
+
+    private ArrayList<Calendar> calendarList = null;
+
+    private int dayNum;
+    private int roomNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,11 +91,120 @@ public class OrderDealActivity extends Activity {
     }
 
     private void initData() {
-
+        initCalendar();
+        initRoomTags();
+        initServiceTags();
     }
 
     private void initListener() {
+        findViewById(R.id.back_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        mLltDateContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OrderDealActivity.this, CalendarActivity.class);
+                if (calendarList != null) {
+                    intent.putExtra("calendarList", calendarList);
+                }
+                startActivityForResult(intent, CalendarActivity.CALENDAR_REQUEST_CODE);
+                overridePendingTransition(R.anim.slide_in_right,
+                        R.anim.slide_out_left);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (RESULT_OK == resultCode) {
+            if(CalendarActivity.CALENDAR_REQUEST_CODE == requestCode){
+                if(null != data){
+                    calendarList = (ArrayList<Calendar>)data.getSerializableExtra("calendarList");
+                    setOrderDate(calendarList);
+                }
+            }
+        }
+    }
+
+    //初始化日期
+    private void initCalendar() {
+        calendarList = new ArrayList<Calendar>();
+
+        mSimpleFormat  = new SimpleDateFormat("yyyy-MM-dd");
+        mChineseFormat = new SimpleDateFormat("MM月dd日");
+
+        Calendar today = Calendar.getInstance();
+        today.setTime(new Date()); //当天
+        calendarList.add(today);
+
+        Calendar tomorrow = Calendar.getInstance();
+        tomorrow.setTime(new Date());
+        tomorrow.add(Calendar.DAY_OF_YEAR, 1); //下一天
+        calendarList.add(tomorrow);
+        setOrderDate(calendarList);
+    }
+
+
+
+    //设置离开和到达的日期
+    private void setOrderDate(ArrayList<Calendar> calendarList){
+        mChineseFormat = new SimpleDateFormat("MM月dd日");
+        Date arrivalDate = calendarList.get(0).getTime();
+        Date leaveDate   = calendarList.get(1).getTime();
+
+        mTvArriveDate.setText(mChineseFormat.format(arrivalDate));
+        mTvLeaveDate.setText(mChineseFormat.format(leaveDate));
+
+        try{
+            dayNum = TimeUtil.daysBetween(arrivalDate, leaveDate);
+            mTvDateTips.setText("共"+dayNum+"晚，在"+mTvLeaveDate.getText()+"13点前退房");
+        }catch (Exception e){
+
+        }
 
     }
+
+    //初始化房间选项标签
+    private void initRoomTags() {
+        String tags[] = {"单早","双早"};
+        for(int i=0;i<2;i++){
+            mRoomTagView.addTag(createTag(i,tags[i],false));
+        }
+    }
+
+    //初始化其他服务标签
+    private void initServiceTags() {
+
+        String tags[] = {"免前台","更多开发中"};
+        for(int i=0;i<2;i++){
+            mServiceTagView.addTag(createTag(i,tags[i],false));
+        }
+    }
+
+    private Tag createTag(int id,String tagstr,boolean isChecked){
+        Tag tag = new Tag(id,tagstr);
+        tag.tagTextColor = Color.parseColor("#000000");
+        tag.layoutColor =  Color.parseColor("#ffffff");
+        tag.layoutColorPress = Color.parseColor("#DDDDDD");
+        //or tag.background = this.getResources().getDrawable(R.drawable.custom_bg);
+        tag.radius = 40f;
+        tag.tagTextSize = 18f;
+        tag.layoutBorderSize = 1f;
+        tag.layoutBorderColor = Color.parseColor("#000000");
+        tag.deleteIndicatorColor =  Color.parseColor("#ff0000");
+        tag.deleteIndicatorSize =  18f;
+        tag.isDeletable = false;
+//        if(isChecked){
+//            tag.deleteIcon = "√";
+//        }else{
+//            tag.deleteIcon = "";
+//        }
+
+        return tag;
+    }
+
 
 }
