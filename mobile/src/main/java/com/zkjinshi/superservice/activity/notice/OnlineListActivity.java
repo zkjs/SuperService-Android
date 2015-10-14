1c2622a0649bbca03eaf933600afd3c6abdecdf3
jimmy5zhang <277936294@qq.com>
2015-10-14 10:58:08 +0800

    1、新增身份枚举值

--- mobile/src/main/java/com/zkjinshi/superservice/activity/notice/OnlineListActivity.java
+++ mobile/src/main/java/com/zkjinshi/superservice/activity/notice/OnlineListActivity.java
@@ -2,8 +2,21 @@ package com.zkjinshi.superservice.activity.notice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 
+import com.google.gson.Gson;
+import com.zkjinshi.base.net.core.WebSocketManager;
+import com.zkjinshi.base.net.observer.IMessageObserver;
+import com.zkjinshi.base.net.observer.MessageSubject;
+import com.zkjinshi.base.net.protocol.ProtocolMSG;
 import com.zkjinshi.superservice.R;
+import com.zkjinshi.superservice.entity.MsgEmpStatus;
+import com.zkjinshi.superservice.entity.MsgEmpStatusRSP;
+import com.zkjinshi.superservice.utils.CacheUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * 员工在线人数列表
@@ -12,14 +25,23 @@ import com.zkjinshi.superservice.R;
  * Copyright (C) 2015 深圳中科金石科技有限公司
  * 版权所有
  */
-public class OnlineListActivity extends AppCompatActivity {
+public class OnlineListActivity extends AppCompatActivity implements IMessageObserver{
 
     private void initView(){
 
     }
 
     private void initData(){
-
+        addObservers();
+        //发送查询客户是否在线请求
+        MsgEmpStatus msgEmpStatus = new MsgEmpStatus();
+        msgEmpStatus.setType(ProtocolMSG.MSG_ShopEmpStatus);
+        msgEmpStatus.setTimestamp(System.currentTimeMillis());
+        msgEmpStatus.setShopid(CacheUtil.getInstance().getShopID());
+        JSONArray empids = new JSONArray();
+        msgEmpStatus.setEmps(empids);
+        String jsonMsgEmpStatus = new Gson().toJson(msgEmpStatus);
+        WebSocketManager.getInstance().sendMessage(jsonMsgEmpStatus);
     }
 
     private void initListeners(){
@@ -33,5 +55,40 @@ public class OnlineListActivity extends AppCompatActivity {
         initView();
         initData();
         initListeners();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        removeObservers();
+    }
+
+    private void addObservers() {
+        MessageSubject.getInstance().addObserver(this, ProtocolMSG.MSG_ShopEmpStatus_RSP);
+    }
+
+    private void removeObservers(){
+        MessageSubject.getInstance().removeObserver(this, ProtocolMSG.MSG_ShopEmpStatus_RSP);
+    }
+
+    @Override
+    public void receive(String message) {
+        if(TextUtils.isEmpty(message)){
+            return ;
+        }
+        Gson gson = null;
+        try {
+            if(null == gson){
+                gson = new Gson();
+            }
+            JSONObject messageObj = new JSONObject(message);
+            int type = messageObj.getInt("type");
+            if (type == ProtocolMSG.MSG_ShopEmpStatus_RSP) {
+                MsgEmpStatusRSP msgEmpStatusRSP = gson.fromJson(message,MsgEmpStatusRSP.class);
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
