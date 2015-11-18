e45aec7d28da1cdedc036baf53045dcfa4723600
JimmyZhang <jimmyzhang144@gmail.com>
2015-11-18 15:55:59 +0800

    新增邀请加入广播透传机制

---
+++ mobile/src/main/java/com/zkjinshi/superservice/receiver/EMessageReceiver.java
@@ -0,0 +1,29 @@
+package com.zkjinshi.superservice.receiver;
+
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.WindowManager;
+
+import com.zkjinshi.base.view.CustomDialog;
+
+/**
+ * 开发者：JimmyZhang
+ * 日期：2015/11/18
+ * Copyright (C) 2015 深圳中科金石科技有限公司
+ * 版权所有
+ */
+public class EMessageReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if(!TextUtils.isEmpty(action) && action.equals("com.zkjinshi.svip.ACTION_INVITE")){
+
+        }
+    }
+
+}
