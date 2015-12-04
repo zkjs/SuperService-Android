cf715758f80ba2a0cc523a05aaa3a52f850024a0
JimmyZhang <jimmyzhang144@gmail.com>
2015-12-04 15:42:23 +0800

    解决图片过多，内存溢出问题

--- mobile/src/main/java/com/zkjinshi/superservice/adapter/ChatAdapter.java
+++ mobile/src/main/java/com/zkjinshi/superservice/adapter/ChatAdapter.java
@@ -473,34 +473,7 @@ public class ChatAdapter extends BaseAdapter {
                     String thumbUrl =  imgBody.getThumbnailUrl();
                     if(!TextUtils.isEmpty(thumbUrl)){
                         ImageLoader.getInstance().displayImage(thumbUrl, vh.img,
-                                imageOptions, new ImageLoadingListener() {
-
-                                    @Override
-                                    public void onLoadingStarted(String imageUri,
-                                                                 View view) {
-                                    }
-
-                                    @Override
-                                    public void onLoadingFailed(String imageUri,
-                                                                View view, FailReason failReason) {
-                                    }
-
-                                    @Override
-                                    public void onLoadingComplete(String imageUri,
-                                                                  View view, Bitmap loadedImage) {
-                                        File file = new File(
-                                                FileUtil.getInstance().getImagePath()
-                                                        + fileName);
-                                        ImageUtil.saveBitmap(loadedImage,
-                                                file.getPath());
-                                        msgCacheMap.put(key,loadedImage);
-                                    }
-
-                                    @Override
-                                    public void onLoadingCancelled(String imageUri,
-                                                                   View view) {
-                                    }
-                                });
+                                imageOptions);
                     }
                 }
             } else {
