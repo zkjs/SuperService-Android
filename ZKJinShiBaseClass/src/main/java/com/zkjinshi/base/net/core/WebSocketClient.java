200e265b5b09dc3d40293685106c8b4d98d72134
JimmyZhang <jimmyzhang144@gmail.com>
2015-12-07 17:12:31 +0800

    1、采用域名替代ip地址 2、去掉SSL单向加密（韩版三星盖世3无法建立通信链接）

--- ZKJinShiBaseClass/src/main/java/com/zkjinshi/base/net/core/WebSocketClient.java
+++ ZKJinShiBaseClass/src/main/java/com/zkjinshi/base/net/core/WebSocketClient.java
@@ -93,11 +93,11 @@ public class WebSocketClient {
 
                     PrintWriter out = new PrintWriter(mSocket.getOutputStream());
                     out.print("GET " + path + " HTTP/1.1\r\n");
-                    out.print("Upgrade: websocket\r\n");
-                    out.print("Connection: Upgrade\r\n");
                     out.print("Host: " + mURI.getHost() + "\r\n");
-                    out.print("Origin: " + origin.toString() + "\r\n");
+                    out.print("Upgrade: WebSocket\r\n");
+                    out.print("Connection: Upgrade\r\n");
                     out.print("Sec-WebSocket-Key: " + secret + "\r\n");
+                    out.print("Origin: " + origin.toString() + "\r\n");
                     out.print("Sec-WebSocket-Version: 13\r\n");
                     out.print("\r\n");
                     out.flush();
