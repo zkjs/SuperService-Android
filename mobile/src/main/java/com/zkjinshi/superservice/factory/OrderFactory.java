229fe335b843be756c6d0921aa823cb5e4dc96f2
winkyqin <winkyqin@163.com>
2016-01-04 11:29:55 +0800

    1. 更新房价非空判断

--- mobile/src/main/java/com/zkjinshi/superservice/factory/OrderFactory.java
+++ mobile/src/main/java/com/zkjinshi/superservice/factory/OrderFactory.java
@@ -65,7 +65,6 @@ public class OrderFactory {
         if(!TextUtils.isEmpty(roomRate)){
             orderDetailBean.setRoomprice(Float.valueOf(roomRate));
         }
-
         orderDetailBean.setImgurl(bookOrderBean.getImage());
         orderDetailBean.setOrderstatus(bookOrderBean.getStatus());
         orderDetailBean.setRemark(bookOrderBean.getRemark());
