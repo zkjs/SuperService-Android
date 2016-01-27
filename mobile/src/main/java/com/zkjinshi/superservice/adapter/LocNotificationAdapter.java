565f8f3e12df69596eb0b363efe7e004cc835be3
winkyqin <winkyqin@163.com>
2016-01-27 15:20:32 +0800

    1. 更新到店通知显示列表

--- mobile/src/main/java/com/zkjinshi/superservice/adapter/LocNotificationAdapter.java
+++ mobile/src/main/java/com/zkjinshi/superservice/adapter/LocNotificationAdapter.java
@@ -44,7 +44,7 @@ public class LocNotificationAdapter extends RecyclerView.Adapter<RecyclerView.Vi
 
     public void setNoticeList(ArrayList<NoticeVo> noticeList) {
         if(null == noticeList){
-            this.noticeList = new ArrayList<NoticeVo>();
+            this.noticeList = new ArrayList<>();
         }else{
             this.noticeList = noticeList;
         }
@@ -56,9 +56,9 @@ public class LocNotificationAdapter extends RecyclerView.Adapter<RecyclerView.Vi
         this.context = activity;
         this.setNoticeList(comingList);
         this.options = new DisplayImageOptions.Builder()
-                .showImageOnLoading(R.mipmap.img_hotel_zhanwei)// 设置图片下载期间显示的图片
-                .showImageForEmptyUri(R.mipmap.img_hotel_zhanwei)// 设置图片Uri为空或是错误的时候显示的图片
-                .showImageOnFail(R.mipmap.img_hotel_zhanwei)// 设置图片加载或解码过程中发生错误显示的图片
+                .showImageOnLoading(R.mipmap.ic_launcher)// 设置图片下载期间显示的图片
+                .showImageForEmptyUri(R.mipmap.ic_launcher)// 设置图片Uri为空或是错误的时候显示的图片
+                .showImageOnFail(R.mipmap.ic_launcher)// 设置图片加载或解码过程中发生错误显示的图片
                 .cacheInMemory(true) // 设置下载的图片是否缓存在内存中
                 .cacheOnDisk(true) // 设置下载的图片是否缓存在SD卡中
                 .build();
