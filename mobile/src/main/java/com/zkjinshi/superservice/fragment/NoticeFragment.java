4a5fcb9187ed4d8278f256b1e77aa7b845f239ee
JimmyZhang <jimmyzhang144@gmail.com>
2016-03-16 14:54:58 +0800

    增加下拉刷新功能、加载底部加载更多

--- mobile/src/main/java/com/zkjinshi/superservice/fragment/NoticeFragment.java
+++ mobile/src/main/java/com/zkjinshi/superservice/fragment/NoticeFragment.java
@@ -54,14 +54,14 @@ import java.util.HashMap;
 public class NoticeFragment extends Fragment {
 
     public static final String TAG = "NoticeFragment";
-    public static final int PAGE_NO = 0;
+    public static int PAGE_NO = 0;
     public static final int PAGE_SIZE = 10;
     private Activity     activity;
     private RecyclerView notityRecyclerView;
     private LinearLayoutManager    notifyLayoutManager;
     private LocNotificationAdapter notificationAdapter;
     private SwipeRefreshLayout     swipeRefreshLayout;
-    private ArrayList<NoticeVo>    noticeList = new ArrayList<NoticeVo>();
+    private ArrayList<NoticeVo>  noticeList = new ArrayList<NoticeVo>();
     private TextView emptyTips;
 
     public static NoticeFragment newInstance() {
@@ -102,7 +102,41 @@ public class NoticeFragment extends Fragment {
             @Override
             public void onRefresh() {
                 noticeList = new ArrayList<NoticeVo>();
-                requestNoticesTask();
+                PAGE_NO = 0;
+                requestNoticesTask(true);
+            }
+        });
+
+        //自动加载更多
+        notityRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            boolean isSlidingToLast = false;
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager)recyclerView.getLayoutManager();
+                if(newState == RecyclerView.SCROLL_STATE_IDLE){
+                    int lastVisibleItem = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+                    int totalItemCount = linearLayoutManager.getItemCount();
+                    if (lastVisibleItem == (totalItemCount -1) && isSlidingToLast) {
+                        //加载更多功能的代码
+                        swipeRefreshLayout.setRefreshing(true);
+                        requestNoticesTask(false);
+                    }
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                //dx用来判断横向滑动方向，dy用来判断纵向滑动方向
+                if(dy > 0){
+                    //大于0表示，正在向右滚动
+                    isSlidingToLast = true;
+                }else{
+                    //小于等于0 表示停止或向左滚动
+                    isSlidingToLast = false;
+                }
             }
         });
 
@@ -178,14 +212,15 @@ public class NoticeFragment extends Fragment {
             return;
         }
         if (locIds.contains(",")) {
-            requestNoticesTask();
+            PAGE_NO = 0;
+            requestNoticesTask(true);
         }
     }
 
     /**
      * 获取多个到店区域通知
      */
-    public void requestNoticesTask() {
+    public void requestNoticesTask(final boolean isRefresh) {
         String locIds = CacheUtil.getInstance().getAreaInfo();
         String shopId = CacheUtil.getInstance().getShopID();
         String noticesUrl = ProtocolUtil.getNoticeUrl(shopId,locIds,""+PAGE_NO,""+PAGE_SIZE);
@@ -218,7 +253,13 @@ public class NoticeFragment extends Fragment {
                     if(null != noticeResponse){
                         int resultCode = noticeResponse.getRes();
                         if(0 == resultCode){
-                            noticeList = noticeResponse.getData();
+                            if(isRefresh){
+                                noticeList = noticeResponse.getData();
+                            }else {
+                                ArrayList<NoticeVo>  requestNoticeList = noticeResponse.getData();
+                                noticeList.addAll(requestNoticeList);
+                            }
+                            PAGE_NO++;
                             notificationAdapter.setNoticeList(noticeList);
                         }else {
                             String resultMsg = noticeResponse.getResDesc();
